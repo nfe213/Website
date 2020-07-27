b15629395899db65a3e45dbc911b1fc0146aa427
Nathan Eads <eadsnf@gmail.com>
2020-07-27 13:02:44 -0400

    Added Sweet Alerts and browser detect. (Currently disabled)

--- browserDetect.js
+++ browserDetect.js
@@ -1,14 +1,8 @@
-
-import swal from 'sweetalert';
-
-$(document).ready(function(){
-  function button() {
-    swal("Hello world!");
-    console.log("Yo");
-  }
-  $("#img-clck").click(button);
-});
-
+console.log('Yo1')
+function sweetalertclick() {
+  console.log('Yo.')
+  swal.fire('Hi there.')
+}
 
 
 var navUserAgent = navigator.userAgent;
@@ -23,21 +17,32 @@ if ((tempVersionOffset=navUserAgent.indexOf("Opera"))!=-1) {
  browserVersion = navUserAgent.substring(tempVersionOffset+6);
  if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1)
    browserVersion = navUserAgent.substring(tempVersionOffset+8);
-} else if ((tempVersionOffset=navUserAgent.indexOf("MSIE"))!=-1) {
+}
+
+else if ((tempVersionOffset=navUserAgent.indexOf("MSIE"))!=-1) {
  browserName = "Microsoft Internet Explorer";
  browserVersion = navUserAgent.substring(tempVersionOffset+5);
-} else if ((tempVersionOffset=navUserAgent.indexOf("Chrome"))!=-1) {
+ alert("We've detected you're using" + browserName + ". Please try switching to Google Chrome, Safari, Microsoft Edge, or Firefox for the site to load properly.");
+}
+
+else if ((tempVersionOffset=navUserAgent.indexOf("Chrome"))!=-1) {
  browserName = "Chrome";
  browserVersion = navUserAgent.substring(tempVersionOffset+7);
-} else if ((tempVersionOffset=navUserAgent.indexOf("Safari"))!=-1) {
+}
+
+else if ((tempVersionOffset=navUserAgent.indexOf("Safari"))!=-1) {
  browserName = "Safari";
  browserVersion = navUserAgent.substring(tempVersionOffset+7);
  if ((tempVersionOffset=navUserAgent.indexOf("Version"))!=-1)
    browserVersion = navUserAgent.substring(tempVersionOffset+8);
-} else if ((tempVersionOffset=navUserAgent.indexOf("Firefox"))!=-1) {
+}
+
+else if ((tempVersionOffset=navUserAgent.indexOf("Firefox"))!=-1) {
  browserName = "Firefox";
  browserVersion = navUserAgent.substring(tempVersionOffset+8);
-} else if ( (tempNameOffset=navUserAgent.lastIndexOf(' ')+1) < (tempVersionOffset=navUserAgent.lastIndexOf('/')) ) {
+}
+
+else if ( (tempNameOffset=navUserAgent.lastIndexOf(' ')+1) < (tempVersionOffset=navUserAgent.lastIndexOf('/')) ) {
  browserName = navUserAgent.substring(tempNameOffset,tempVersionOffset);
  browserVersion = navUserAgent.substring(tempVersionOffset+1);
  if (browserName.toLowerCase()==browserName.toUpperCase()) {
@@ -50,5 +55,3 @@ if ((tempVersion=browserVersion.indexOf(";"))!=-1)
    browserVersion=browserVersion.substring(0,tempVersion);
 if ((tempVersion=browserVersion.indexOf(" "))!=-1)
    browserVersion=browserVersion.substring(0,tempVersion);
-
-alert("BrowserName = " + browserName + "\n" + "Version = " + browserVersion);
